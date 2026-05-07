a87aaf76688e98351efb99a10529a7e78f14ccd9
Shefler Alexandr <sasha.shefler9@gmail.com>
2026-05-08 00:05:46 +0500

    KAN-5 feat(user): implement core User entity and basic authentication flow
          - Added User schema with fullName, phone, email, password, and role fields
          - Enforced unique indexes for phone and email
          - Implemented Argon2 password hashing before saving users
          - Added POST /auth/register endpoint for client registration
          - Added POST /auth/login endpoint with credential validation
          - Hid password field in API responses
          - Standardized error handling for existing users and invalid credentials
          - Ensured basic auth flow is functional and secure

---
+++ backend/src/model/User.ts
@@ -0,0 +1,78 @@
+import mongoose, { Schema, Model, HydratedDocument } from "mongoose";
+import argon2 from "argon2";
+import type { UserFields } from "@/types/types.js";
+
+interface UserMethods {
+    checkPassword(password: string): Promise<boolean>;
+}
+
+type UserModel = Model<UserFields, {}, UserMethods>;
+
+export type UserDocument = HydratedDocument<UserFields, UserMethods>;
+
+const UserSchema = new Schema<UserFields, UserModel, UserMethods>(
+    {
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        phone: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+
+        role: {
+            type: String,
+            enum: ["ADMIN", "MANAGER", "CLIENT"],
+            default: "CLIENT",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+UserSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
+    this.password = await argon2.hash(this.password, {
+        type: argon2.argon2id,
+        memoryCost: 2 ** 16,
+        timeCost: 3,
+    });
+});
+
+UserSchema.methods.checkPassword = function (password: string) {
+    return argon2.verify(this.password, password);
+};
+
+UserSchema.set("toJSON", {
+    transform: (_doc, ret, _options) => {
+        const {password, ...rest} = ret;
+        return rest;
+    }
+});
+
+
+const User = mongoose.model<UserFields, UserModel>("User", UserSchema);
+
+export default User;
