3b239179444b6e341fdbb67c0371431c86982058
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-05-07 18:38:38 +0600

    feat: initial backend setup with project architecture and dependencies

---
+++ backend/src/config.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const rootPath = path.dirname(__filename);
+
+const config = {
+  rootPath,
+  publicPath: path.join(rootPath, '../public'),
+  port: process.env.PORT ?? 3000,
+  db: 'mongodb://localhost/virgin-travel',
+  refreshJWTSecret: process.env.REFRESH_SECRET_JWT ?? 'refresh_secret',
+  accessJWTSecret: process.env.ACCESS_SECRET_JWT ?? 'access_secret',
+};
+
+export default config;
---
+++ backend/src/index.ts
@@ -0,0 +1,41 @@
+import express, { type Express, type Request, type Response } from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import config from './config.js';
+import apiRouter from './routes/api.route.js';
+
+const app: Express = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(express.json());
+dotenv.config();
+app.use('/api', apiRouter);
+
+app.use((_req: Request, res: Response) => {
+  return res.status(404).json({
+    error: 'Not Found',
+  });
+});
+
+const run = async () => {
+  await mongoose.connect(config.db);
+
+  app.listen(config.port, () => {
+    console.log(`http://localhost:${config.port}/api`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+run().catch((error) => console.error(error));
