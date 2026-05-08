e9bf41654cf1adee62c4b78ddf2f9b8285d8285f
Shefler Alexandr <sasha.shefler9@gmail.com>
2026-05-08 22:26:58 +0500

    KAN-14 feat(news): add News model and full CRUD API

--- backend/src/routes/api.route.ts
+++ backend/src/routes/api.route.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import usersRouter from './users/users.js';
 import categoriesRouter from './categories/categories.js';
+import newsRouter from "@/routes/news/news.js";
 
 
 const apiRouter = Router();
 
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/categories', categoriesRouter);
+apiRouter.use('/news', newsRouter);
 
 export default apiRouter;
---
+++ backend/src/routes/news/news.ts
@@ -0,0 +1,199 @@
+import express from "express";
+import mongoose from "mongoose";
+import News from "@/model/New/News.js";
+import auth, {RequestWithUser} from "@/middlewares/auth.js";
+import permit from "@/middlewares/permit.js";
+import {imagesUpload} from "@/middlewares/multer.js";
+import {NewsFields} from "@/types/news.types.js";
+
+const newsRouter = express.Router();
+
+newsRouter.get("/", async (req, res, next) => {
+    try {
+        const query: {
+            isPublished: boolean;
+            tags?: string[];
+        } = {
+            isPublished: true,
+        };
+
+        const tags = req.query.tags;
+
+        if (typeof tags === "string" && tags.length > 0) {
+            query.tags = tags.split(",");
+        }
+
+        const news = await News.find(query)
+            .sort({ createdAt: -1 })
+            .populate("author", "fullName");
+
+        res.send(news);
+    } catch (e) {
+        next(e);
+    }
+});
+
+newsRouter.get("/:id", async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).send({
+                error: "Невалидный ID",
+            });
+        }
+
+        const infoNew = await News.findById(id);
+
+        if (!infoNew) {
+            return res.status(404).send({
+                error: "Новость не найдена",
+            });
+        }
+
+        res.send(infoNew);
+    } catch (error) {
+        next(error);
+    }
+});
+
+newsRouter.post("/", auth, permit("ADMIN", "MANAGER"), imagesUpload.single("image"), async (req, res, next) => {
+    try {
+        const { title, content, tags } = req.body;
+
+        const { user } = req as RequestWithUser;
+
+        const parsedTags =
+            typeof tags === "string"
+                ? tags
+                    .split(",")
+                    .map((t) => t.trim())
+                    .filter(Boolean)
+                : [];
+
+        const news = new News({
+            title,
+            content,
+            image: req.file ? "images/" + req.file.filename : null,
+            tags: parsedTags,
+            author: user._id,
+        });
+
+        const savedNews = await news.save();
+
+        res.send({
+            message: "НОВОСТЬ СОЗДАНА",
+            news: savedNews,
+        });
+    } catch (e) {
+        if (e instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send({
+                error: "Ошибка валидации",
+                details: e.errors,
+            });
+        }
+        next(e);
+    }
+});
+
+newsRouter.delete("/:id", auth, permit("ADMIN", "MANAGER"), async (req, res, next) => {
+    const { id } = req.params;
+    const isValidId = mongoose.Types.ObjectId.isValid(id as string);
+
+    if (!id || !isValidId) {
+        return res.status(400).send({ error: "Неверный ID" });
+    }
+
+    try {
+        const { deletedCount } = await News.deleteOne({ _id: id });
+        if (!deletedCount) {
+            return res.status(404).send({ error: "Новость не найдена" });
+        }
+        return res.send({ message: "Новость удалена" });
+    } catch (e) {
+        next(e);
+    }
+});
+
+newsRouter.patch(
+    '/:id/isPublished',
+    auth,
+    permit("ADMIN", "MANAGER"),
+    async (req, res, next) => {
+        const { id } = req.params;
+        const isValidId = mongoose.Types.ObjectId.isValid(id as string);
+
+        if (!id || !isValidId) {
+            return res.status(400).send({ error: "Неверный ID" });
+        }
+
+        try {
+            const news = await News.findById(id);
+            if (!news)
+                return res.status(404).send({ error: "Новость не найдена" });
+
+            news.isPublished = !news.isPublished;
+            await news.save();
+            return res.send(news);
+        } catch (e) {
+            next(e);
+        }
+    },
+);
+
+newsRouter.patch("/:id/edit", auth, permit("ADMIN", "MANAGER"), imagesUpload.single("image"), async (req, res, next) => {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id as string)) {
+            return res.status(400).send({
+                error: "Неверный ID",
+            });
+        }
+
+        try {
+            const news = await News.findById(id);
+
+            if (!news) {
+                return res.status(404).send({
+                    error: "Новость не найдена",
+                });
+            }
+
+            const { title, content, tags } = req.body;
+
+            const updateData: Partial<NewsFields> = {};
+
+            if (title) updateData.title = title;
+            if (content) updateData.content = content;
+
+            if (typeof tags === "string") {
+                updateData.tags = tags
+                    .split(",")
+                    .map(t => t.trim())
+                    .filter(Boolean);
+            }
+
+            if (req.file) {
+                updateData.image = "images/" + req.file.filename;
+            } else {
+                updateData.image = null;
+            }
+
+            const updated = await News.findByIdAndUpdate(id, updateData, {
+                returnDocument: "after",
+                runValidators: true,
+            });
+
+            res.send({
+                message: "НОВОСТЬ ОБНОВЛЕНА",
+                news: updated,
+            });
+        } catch (e) {
+            next(e);
+        }
+    }
+);
+
+
+
+export default newsRouter;
