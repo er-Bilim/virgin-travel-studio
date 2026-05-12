891fe293323f1714d0e358a3da0a70199554a347
Fedulov Danil <danya7777kg@gmail.com>
2026-05-12 21:08:18 +0600

    KAN-16_Order_entity Created order model and interface

---
+++ backend/src/types/orders.types.ts
@@ -0,0 +1,12 @@
+import {Document, Types} from 'mongoose';
+
+export type OrderStatus = 'NEW' | 'IN_PROGRESS' | 'CONTRACT_PENDING' | 'COMPLETED' | 'REJECTED';
+
+export interface IOrder extends Document {
+  tourSetId: Types.ObjectId;
+  clientName: string;
+  clientPhone: string;
+  status: OrderStatus;
+  rejectionReason: string | null;
+  managerId?: Types.ObjectId | null;
+}
