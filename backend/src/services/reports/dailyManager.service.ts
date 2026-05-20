cc5f89d85665c51c66d9748db6febbc5d4cfa2e8
Shefler Alexandr <sasha.shefler9@gmail.com>
2026-05-20 15:46:40 +0500

    KAN-36 feat(reports): add excel export for tour roster and daily manager reports
           - implemented ExcelJS builders for tour roster and daily manager reports
           - added reusable applyExcelStyles helper for consistent formatting
           - added aggregation-based daily manager statistics (by status and revenue)
           - added tour roster report with populated tour, manager, and formatted dates
           - fixed Excel file generation and download response handling
           - improved report structure and separation of service/builder logic

---
+++ backend/src/services/reports/dailyManager.service.ts
@@ -0,0 +1,99 @@
+import type {DailyManagerRow, GetParams} from "@/types/reports.types.js";
+import {Types} from "mongoose";
+import Order from "@/model/order/Order.js";
+
+export const getDailyManagerReportData = async ({
+                                                    from,
+                                                    to,
+                                                    managerId,
+                                                }: GetParams): Promise<DailyManagerRow[]> => {
+
+    const match: Record<string, unknown> = {};
+
+    if (from || to) {
+        match.createdAt = {};
+
+        if (from) {
+            (match.createdAt as Record<string, Date>).$gte = new Date(from);
+        }
+
+        if (to) {
+            (match.createdAt as Record<string, Date>).$lte = new Date(to);
+        }
+    }
+
+    if (managerId) {
+        match.managerId = new Types.ObjectId(managerId);
+    }
+
+    const result = await Order.aggregate([
+        { $match: match },
+
+        {
+            $lookup: {
+                from: 'users',
+                localField: 'managerId',
+                foreignField: '_id',
+                as: 'manager',
+            },
+        },
+        { $unwind: '$manager' },
+
+        {
+            $lookup: {
+                from: 'toursets',
+                localField: 'tourSetId',
+                foreignField: '_id',
+                as: 'tourSet',
+            },
+        },
+        { $unwind: '$tourSet' },
+
+        {
+            $group: {
+                _id: '$manager._id',
+
+                manager: { $first: '$manager.fullName' },
+
+                newOrders: {
+                    $sum: { $cond: [{ $eq: ['$status', 'NEW'] }, 1, 0] },
+                },
+
+                inProgress: {
+                    $sum: { $cond: [{ $eq: ['$status', 'IN_PROGRESS'] }, 1, 0] },
+                },
+
+                completed: {
+                    $sum: { $cond: [{ $eq: ['$status', 'COMPLETED'] }, 1, 0] },
+                },
+
+                rejected: {
+                    $sum: { $cond: [{ $eq: ['$status', 'REJECTED'] }, 1, 0] },
+                },
+
+                revenue: {
+                    $sum: {
+                        $cond: [
+                            { $eq: ['$status', 'COMPLETED'] },
+                            '$tourSet.price',
+                            0,
+                        ],
+                    },
+                },
+            },
+        },
+
+        {
+            $project: {
+                _id: 0,
+                manager: 1,
+                newOrders: 1,
+                inProgress: 1,
+                completed: 1,
+                rejected: 1,
+                revenue: 1,
+            },
+        },
+    ]);
+    return result as DailyManagerRow[];
+};
---
+++ backend/src/services/reports/tourRoster.service.ts
@@ -0,0 +1,40 @@
+import Order from "@/model/order/Order.js";
+import type {PopulatedOrder} from "@/types/reports.types.js";
+
+
+export const getTourRosterData = async (tourSetId: string) => {
+    const orders = await Order.find({ tourSetId, status: "COMPLETED" })
+        .populate({
+            path: 'tourSetId',
+            populate: {
+                path: 'tourId',
+            },
+        })
+        .populate('managerId')
+        .lean() as unknown as PopulatedOrder[];
+
+    console.log(orders[0]);
+    const formatDate = (date: Date) => {
+        const d = new Date(date);
+
+        const day = String(d.getDate()).padStart(2, '0');
+        const month = String(d.getMonth() + 1).padStart(2, '0');
+        const year = d.getFullYear();
+
+        return `${day}.${month}.${year}`;
+    };
+
+    return  orders.map(order => ({
+        clientName: order.clientName,
+        clientPhone: order.clientPhone,
+        status: order.status,
+
+        tour: order.tourSetId?.tourId.title,
+        dates: `${formatDate(order.tourSetId.startDate)} - ${formatDate(order.tourSetId.endDate)}`,
+        hotel: order.tourSetId?.hotelName,
+
+        manager: order.managerId?.fullName,
+        sum: order.tourSetId?.price,
+    }));
+};
+
