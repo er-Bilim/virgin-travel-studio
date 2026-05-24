90a31df67718dca7dc5cc345130fa5b4b0c1e3fe
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-05-24 17:58:54 +0600

    feat(frontend): implement infinite scroll for reviews with load more button

--- frontend/app/(public)/tourSets/[slug]/page.tsx
+++ frontend/app/(public)/tourSets/[slug]/page.tsx
@@ -29,7 +29,7 @@ import { buildTourInquiryMessage, openWhatsApp } from '@/lib/whatsapp';
 import { FaWhatsapp } from 'react-icons/fa';
 import Review from '@/components/public/reviews/Review';
 import { Spinner } from '@/components/ui/spinner';
-import { useGetReviews } from '@/lib/hooks/reviewHooks';
+import { useInfiniteReviews } from '@/lib/hooks/reviewHooks';
 
 export default function TourSetPage() {
   const params = useParams();
@@ -37,7 +37,15 @@ export default function TourSetPage() {
 
   const { data, isLoading, isError } = useOneTourSet(slug);
   const tourId = data?.tourId?._id;
-  const {data: reviews, isLoading: isLoadingReviews, isError: isReviewsError } = useGetReviews(tourId)
+  const {
+    data: reviewsData,
+    isLoading: isLoadingReviews,
+    isError: isReviewsError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteReviews(tourId);
+  const reviews = reviewsData?.pages.flatMap((page) => page.reviews) ?? [];
 
   const [isOrderOpen, setIsOrderOpen] = useState(false);
 
@@ -95,7 +103,7 @@ export default function TourSetPage() {
 
     if (!reviews?.length) {
       return (
-        <div className='border-1 border-[var(--border)] rounded-xl p-4 text-gray-600 h-35 flex flex-col justify-center gap-4 items-center'> 
+        <div className="border-1 border-[var(--border)] rounded-xl p-4 text-gray-600 h-35 flex flex-col justify-center gap-4 items-center">
           <p className="text-gray-400 bg-gray-200 p-3 rounded-full">
             <MessageSquareDashed />
           </p>
@@ -113,6 +121,22 @@ export default function TourSetPage() {
     );
   };
 
+  const renderRating = () => {
+    return (
+      <div className="flex gap-1 items-center">
+        <Star className="stroke-2 stroke-yellow-400 text-yellow-400" />
+        <span className="text-[var(--primary)] font-semibold">
+          {tour.rating > 0 ? tour.rating : 'нет оценок :('}
+        </span>
+        <Dot className="stroke-1 size-4" />
+        <p className="font-semibold flex gap-1">
+          {tour.ratingCount > 0 ? tour.ratingCount : 'нет'}
+          <span className="font-normal">отзывов</span>
+        </p>
+      </div>
+    );
+  };
+
   return (
     <>
       {data._id && (
@@ -122,16 +146,16 @@ export default function TourSetPage() {
           tourSetId={data._id}
         />
       )}
-      <section aria-labelledby="tour-title" className="mt-5">
+      <section aria-labelledby="tour-title" className="mt-5 relative">
         {/* <nav aria-label="Хлебные крошки">Хлебные крошки</nav> */}
-        <div className="mb-4 flex items-center gap-4">
+        <div className="flex items-center gap-4 absolute z-1 top-3 left-3 text-xs">
           {set.isHot && (
-            <p className="flex gap-2 bg-red-100 text-red-500 border-1 border-red-500 rounded-4xl uppercase font-semibold px-4 py-2 text-sm items-center">
-              <Flame className="size-5" />
+            <p className="flex gap-2 bg-blur bg-red-500 text-red-50 border-1 border-red-500 rounded-4xl uppercase font-semibold px-4 py-2 items-center">
+              <Flame className="size-4" />
               Горящий
             </p>
           )}
-          <p className="flex gap-2 bg-slate-100 text-slate-500 border-1 border-slate-500 rounded-4xl uppercase font-semibold px-4 py-2 text-sm">
+          <p className="flex gap-2 bg-slate-100 text-slate-500 border-1 border-slate-500 rounded-4xl uppercase font-semibold px-4 py-2">
             {tour.category.title}
           </p>
         </div>
@@ -162,17 +186,7 @@ export default function TourSetPage() {
               <span>{data.hotelLocation}</span>
             </div>
 
-            <div className="flex gap-1 items-center">
-              <Star className="stroke-2 stroke-yellow-400 text-yellow-400" />
-              <span className="text-[var(--primary)] font-semibold">
-                {tour.rating > 0 ? tour.rating : 'нет оценок :('}
-              </span>
-              <Dot className="stroke-1 size-4" />
-              <p className="font-semibold flex gap-1">
-                {tour.ratingCount > 0 ? tour.ratingCount : 'нет'}
-                <span className="font-normal">отзывов</span>
-              </p>
-            </div>
+            {renderRating()}
           </div>
         </header>
       </section>
@@ -235,16 +249,25 @@ export default function TourSetPage() {
           </section>
 
           <section className="border-t border-border pt-10">
-            <CreateReviewForm tourId={tourId}/>
+            <CreateReviewForm tourId={tourId} />
 
-            <h2
-              id="reviews-title"
-              className="font-semibold text-[1.3rem] mt-5 border-t border-border pt-5"
-            >
-              Отзывы путешественников
-            </h2>
-
+            <div className="flex items-center direction-row  border-t border-border pt-5 mt-8 justify-between">
+              <h2 id="reviews-title" className="font-semibold text-[1.3rem] ">
+                Отзывы путешественников
+              </h2>
+              {renderRating()}
+            </div>
             <div className="flex flex-col gap-3 mt-6">{renderReviews()}</div>
+            {hasNextPage && (
+              <button
+                aria-label='загрузить еще отзывов'
+                className="text-center border-1 border-[var(--silver)] rounded-lg p-3 mt-5 w-full font-semibold cursor-pointer"
+                onClick={() => fetchNextPage()}
+                disabled={isFetchingNextPage}
+              >
+                {isFetchingNextPage ? <Spinner /> : `Загрузить еще`}
+              </button>
+            )}
           </section>
         </div>
 
