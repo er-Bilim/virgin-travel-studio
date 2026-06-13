0ab18cb7fe7a38ea5cc5ad23ab4349d9b6d28ef7
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-06-13 23:53:40 +0600

    feat(frontend): add custom tour page

---
+++ frontend/app/(public)/tours/custom/page.tsx
@@ -0,0 +1,11 @@
+import CustomTourForm from "@/components/shared/order/customTourForm/CustomTourForm";
+
+const CustomTourPage = () => {
+  return (
+    <>
+      <CustomTourForm/>
+    </>
+  );
+};
+
+export default CustomTourPage;
