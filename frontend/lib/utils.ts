f8bd69a5e4cddaa0e75c968be89f30d15aaad05e
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-06-27 23:41:25 +0600

    style(frontend): update homepage section styles

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -45,13 +45,6 @@ export const formatDayAndMonthWords = (date: string, isSlice?: boolean): {day: s
   return { day, month, year }
 }
 
-export const getYearFullNumber = (date: string) => {
-  const formatDate = dayjs(date);
-
-  const year = formatDate.format('YYYY');
-  return year;
-};
-
 export const formatToReadablePrice = (
   priceParam: number,
 ): { price: string; currency: string } => {
