6ce9e17938fadc0a366bed1d1d4fe57ca8711a68
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-07-23 23:20:54 +0600

    fix config vitest

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: './vitest.setup.ts',
-    include: ['/*.{test,spec}.{ts,tsx}'],
+    include: ['**/*.{test,spec}.{ts,tsx}'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
